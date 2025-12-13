44ac51103491aea21b284990d616357f0e75a78f
ibrhmliasim <asim.ibrahimov02@gmail.com>
2025-12-13 22:34:59 +0900

    chore: initial project architecture

---
+++ components/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+// IMPORT start
+
+// IMPORT end
+
+// EXPORT start
+    export function HomePage() {
+    return (
+        <main className="container mx-auto py-10">
+            <h1 className="text-4xl font-bold mb-6">
+                PLUSH
+            </h1>
+
+        </main>
+    );
+    }
+// EXPORT end
